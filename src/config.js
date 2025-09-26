c51f23463d7a579161191837dd2e956cf48b265b
Jonson7878 <d.jonson7878@gmail.com>
2025-09-26 10:06:25 +0530

    Update SERVER_BASE_URL to point to the correct production server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const SERVER_BASE_URL = 'https://gatherly-35q6.onrender.com';
+const SERVER_BASE_URL = 'https://gatherly-87jr.onrender.com';
 
 const API_BASE = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
